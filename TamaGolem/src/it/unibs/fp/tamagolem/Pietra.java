9c7431112bfd72aa39f850c252379b375efd77fd
C4moX <filipcamos@gmail.com>
2021-05-05 18:36:33 +0200

    Pietre

--- TamaGolem/src/it/unibs/fp/tamagolem/Pietra.java
+++ TamaGolem/src/it/unibs/fp/tamagolem/Pietra.java
@@ -1,5 +1,30 @@
 package it.unibs.fp.tamagolem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Pietra {
 
+    private Map<String, Integer> scorta_comune = new HashMap<>();
+
+    public int calcolaPietre() {
+        return (Grafo.getNodi().size() + 1) / 3 + 1;
+    }
+
+    public void riempi_scorta(Map<String, Integer> scorta_comune) {
+
+        int totale = calcolaPietre();
+        for(int i= 0; i< Grafo.getNodi().size(); i++){
+            scorta_comune.put(Grafo.getNodi().get(i).getNome(), totale);
+        }
+    }
+
+
+    public Map<String, Integer> getScorta_comune() {
+        return scorta_comune;
+    }
+
+    public void setScorta_comune(Map<String, Integer> scorta_comune) {
+        this.scorta_comune = scorta_comune;
+    }
 }
